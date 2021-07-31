5537b488ab82f240cffb7df9538e39a99e5aa4e0
Gaven <gavennowak20@gmail.com>
2021-07-31 15:01:53 -0400

    added first tile map

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovementCommand();
+    }
+    public void PlayerMovementCommand()
+    {
+        Vector2 pos = gameObject.transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position = new Vector2(pos.x, pos.y + (Time.deltaTime * playerSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = new Vector2(pos.x - (Time.deltaTime * playerSpeed), pos.y);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position = new Vector2(pos.x, pos.y - (Time.deltaTime * playerSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position = new Vector2(pos.x + (Time.deltaTime * playerSpeed), pos.y);
+        }
+    }
+}
